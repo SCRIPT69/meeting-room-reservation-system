c2d79c108ac7679b48523a107f223f79ee7915b1
nam <nguyep12@student.cvut.cz>
2026-04-04 18:05:29 +0200

    Začít s něčím

---
+++ src/test/java/cz/cvut/fel/ts1/reservationapp/ReservationServiceMockitoTest.java
@@ -0,0 +1,16 @@
+package cz.cvut.fel.ts1.reservationapp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+
+public class ReservationServiceMockitoTest {
+    @BeforeEach
+    public void setUp()
+    {
+
+    }
+
+    //@Mock
+
+
+}
